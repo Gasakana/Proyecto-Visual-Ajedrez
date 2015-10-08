9cc2a28e95ed4700fc04dd80b30adb67c76091f8
sirujano <eduardo.formanti.a@gmail.com>
2015-10-08 20:24:11 +0200

    torre añadida

--- frmTablero/Piezas/Torre.vb
+++ frmTablero/Piezas/Torre.vb
@@ -8,20 +8,48 @@
     Public Function mover(ByVal nCasillas(,) As Integer, ByVal filaInicial As Integer, ByVal columnaInicial As Integer,
                           ByVal filaFinal As Integer, ByVal columnaFinal As Integer)
 
+        If columnaFinal > columnaInicial Then
+            For y = columnaInicial To columnaFinal
+                If nCasillas(filaInicial, y) <> 0 Then
+                    'Return 1
+                Else
+                    Return 0
+                End If
 
-        For y = 0 To columnaFinal
-            If nCasillas(filaInicial, y) <> 0 Then
-                Return 1
-            End If
-        Next
 
-        For x = 0 To filaFinal
-            If nCasillas(x, columnaInicial) <> 0 Then
-                Return 1
-            End If
-        Next
+            Next
+        ElseIf columnaFinal < columnaInicial
+            For y = columnaFinal To columnaInicial
+                If nCasillas(filaInicial, y) <> 0 Then
+                    'Return 1
+                Else
+                    Return 0
+                End If
+            Next
 
-        Return 0
+        End If
+
+        If filaFinal > filaInicial Then
+            For x = filaInicial To filaFinal
+                If nCasillas(x, columnaInicial) <> 0 Then
+                    'Return 1
+                Else
+                    Return 0
+                End If
+            Next
+        ElseIf filaFinal < filaInicial
+            For x = filaFinal To filaInicial
+                If nCasillas(x, columnaInicial) <> 0 Then
+                    'Return 1
+                Else
+                    Return 0
+                End If
+            Next
+
+        End If
+
+
+        Return 1
 
     End Function
 
